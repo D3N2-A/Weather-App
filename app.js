d3fc923912be298c7fb4eb7f6d0da5e1da402749
D3N2-A <anmolsharma999937@gmail.com>
2022-06-03 15:10:45 +0530

    GET success

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { appendFile } = require("fs");
 const port = 3000;
 const https = require("https");
-
+const app = express();
 app.get("/", function (req, res) {
   console.log("Server is up and running");
+});
+
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?q=hamirpur&appid=4720aa48fa8d52f9ce9ba5125f37c5f8&units=metric";
+
+https.get(url, function (response) {
+  console.log(response);
 });
 
 app.listen(port, function () {
