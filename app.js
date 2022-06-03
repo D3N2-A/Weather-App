2d8dec1e48e10bd2bf2a331a5ce39c56077df4fa
D3N2-A <anmolsharma999937@gmail.com>
2022-06-03 15:29:13 +0530

    temp & description

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ const url =
   "https://api.openweathermap.org/data/2.5/weather?q=hamirpur&appid=4720aa48fa8d52f9ce9ba5125f37c5f8&units=metric";
 
 https.get(url, function (response) {
-  console.log(response);
+  console.log(response.statusCode);
+  response.on("data", function (data) {
+    const weatherData = JSON.parse(data);
+    // console.log(weatherData);
+    const temp = weatherData.main.temp;
+    console.log(temp);
+    var weatherDescription = weatherData.weather[0].description;
+    console.log(weatherDescription);
+  });
 });
 
 app.listen(port, function () {
